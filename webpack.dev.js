b38f2f786196a24b7a66e8d79656a59d8161c51e
moocifur <davidkttran@gmail.com>
2024-12-12 20:00:57 -0800

    template updated and working

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,19 @@
+// webpack.dev.js
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "eval-source-map",
+  devtool: "inline-source-map", // Source maps for debugging
   devServer: {
-    watchFiles: ["./src/template.html"],
+    static: false, // No need to serve a static folder since HTML is in memory
+    watchFiles: {
+      paths: ["./src/**/*.html", "./src/**/*.css"], // Watch HTML and CSS
+      options: {
+          ignored: /node_modules/, // Exclude node_modules
+      },
+    },
+    open: true, // Automatically open browser
+    hot: true,  // Enable Hot Module Replacement
   },
 });
